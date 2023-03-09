f951c731a886153202d1e45785a86e5d724d24e0
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-03-09 17:01:01 +0100

    Added default Key4hep package configurations to be updated

---
+++ packages/ddmarlinpandora/package.py
@@ -0,0 +1,39 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
+
+
+class Ddmarlinpandora(CMakePackage, Ilcsoftpackage):
+    """Interface between Marlin and PandoraPFA."""
+
+    url      = "https://github.com/iLCSoft/DDMarlinPandora/archive/v00-11.tar.gz"
+    homepage      = "https://github.com/iLCSoft/DDMarlinPandora/archive/v00-11.tar.gz"
+    git      = "https://github.com/iLCSoft/DDMarlinPandora.git"
+
+    maintainers = ['vvolkl']
+
+    version('master', branch='master')
+    version('0.12', sha256='4f90c2ef240c2fa1f293498bf35201d1337651f8847d53da7124a61091bb504e')
+    version('0.11', sha256='92410186209508091e0a8e330986283ffb32e40fd7195d10aad1a6a2e953f3ee')
+
+    depends_on('ilcutil')
+    depends_on('marlinutil')
+    depends_on('marlin')
+    depends_on('pandorasdk')
+    depends_on("pandorapfa")
+    depends_on("lccontent")
+    depends_on("larcontent")
+    depends_on('dd4hep')
+    depends_on('marlintrk')
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libDDMarlinPandora.so")
+
+    def cmake_args(self):
+        # C++ Standard
+        return [
+            '-DCMAKE_CXX_STANDARD=%s' % self.spec['root'].variants['cxxstd'].value
+        ]
---
+++ packages/lctuple/package.py
@@ -0,0 +1,37 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
+
+
+class Lctuple(CMakePackage, Ilcsoftpackage):
+    """Marlin package that creates a ROOT TTree with a column wise ntuple from LCIO collections."""
+
+    url      = "https://github.com/iLCSoft/LCTuple/archive/v01-12.tar.gz"
+    homepage = "https://github.com/iLCSoft/LCTuple"
+    git      = "https://github.com/iLCSoft/LCTuple.git"
+
+    maintainers = ['vvolkl']
+
+    version('master', branch='master')
+    version('1.14', sha256='7088a6923c4e4c3ac327965c8d41f53eb1403134a9df52e43635fa5eaef48581')
+    version('1.13', sha256='35f2ff3d4b89a3fd7e87f6f5c9fec2178afec26ae8c89d30a5b0bcf113d2107f')
+    version('1.12', sha256='e0e7c4c86f257027a7e9b1c42438087a7b0919964f9719080be25df8a0f95968')
+
+
+    depends_on('ilcutil')
+    depends_on('marlin')
+    depends_on('root')
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libLCTuple.so")
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s'
+                    % self.spec['root'].variants['cxxstd'].value)
+        args.append('-DBUILD_TESTING=%s' % self.run_tests)
+        return args
