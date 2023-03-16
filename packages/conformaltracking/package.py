09b53e795cd8c649fb4d15c92e0cc160f8f1e99b
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-03-16 08:46:36 +0100

    Fixed URLs and checksums for ILCSoft packages

--- packages/conformaltracking/package.py
+++ packages/conformaltracking/package.py
@@ -4,24 +4,25 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
+from spack.pkg.mucoll.mucoll_stack import MCIlcsoftpackage
 
 
-class Conformaltracking(CMakePackage, Ilcsoftpackage):
+class Conformaltracking(CMakePackage, MCIlcsoftpackage):
     """Package for running pattern recognition based on conformal mapping
        and cellular automaton. This is not tied to a given geometry, but
        has been developed for the CLIC detector model 2015."""
 
     homepage = "https://github.com/MuonColliderSoft/ConformalTracking"
     git      = "https://github.com/MuonColliderSoft/ConformalTracking.git"
-    url      = "https://github.com/MuonColliderSoft/ConformalTracking/archive/refs/tags/v01-11-MC.tar.gz"
+    url      = "https://github.com/MuonColliderSoft/ConformalTracking/archive/refs/tags/v01-12-MC.tar.gz"
 
 
     maintainers = ['gianelle', 'pandreetto']
 
     version('master', branch='master')
-    version('1.11',   sha256='5ee8d3062cc2a3ffc68dd14aa8d910283e8af4f5')
-    version('1.10',   sha256='c61f9e231f86910685cb647fcd507d55c905e3b6')
+    version('1.12',   sha256='676d20c3f6b6c03910377d0cb241120567cd6c6bd4fbdd0df708965302872e53')
+    version('1.11',   sha256='ea5a8e600546f4a67b555c89bb4f60bd95e6fab2259fe72af78ca865cc76819a')
+    version('1.10',   sha256='fa3d8c12e92dc0748bb427fe3a388b8d500acb5c0e171736068fffe2135b4b7d')
 
     depends_on('ilcutil')
     depends_on('root')
