74293c48472b3ab341537ff2f68689dfa8cb935c
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-06-22 00:55:12 +0200

    Updated MuonCVXDDigitiser url and added a cmake patch

--- packages/muoncvxddigitiser/package.py
+++ packages/muoncvxddigitiser/package.py
@@ -3,17 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.pkg.mucoll.mucoll_stack import MCIlcsoftpackage
+from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
 
 
-class Muoncvxddigitiser(CMakePackage, MCIlcsoftpackage):
+class Muoncvxddigitiser(CMakePackage, Ilcsoftpackage):
     """Realistic digitiser of pixelated sensors for Muon Collider"""
 
     homepage = "https://github.com/MuonColliderSoft/MuonCVXDDigitiser"
     git      = "https://github.com/MuonColliderSoft/MuonCVXDDigitiser.git"
     url      = "https://github.com/MuonColliderSoft/MuonCVXDDigitiser/archive/refs/tags/v00-01.tar.gz"
 
-    version('master', branch='master')
+    version("master", branch="master")
     version("0.1",    sha256="b4fe817025aeda01e0d503a91a5988b4c1d906dfcb02d2a505f013f8de90efc0")
     
 
@@ -23,6 +23,9 @@ class Muoncvxddigitiser(CMakePackage, MCIlcsoftpackage):
     depends_on('dd4hep')
     depends_on('lcio')
     depends_on('clhep')
+
+    # Defining patches
+    patch("cmake_v0.1.patch", when='@=0.1')
 
     def setup_run_environment(self, spack_env):
         spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libMuonCVXDDigitiser.so")
