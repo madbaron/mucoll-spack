bd8306131311576f6eb0d7575d40493c896694e1
Federico Meloni <federico.meloni@cern.ch>
2024-12-05 14:47:45 +0100

    marlintrack

--- packages/marlintrkprocessors/package.py
+++ packages/marlintrkprocessors/package.py
@@ -10,14 +10,15 @@ from spack.pkg.mucoll.mucoll_stack import MCIlcsoftpackage
 class Marlintrkprocessors(CMakePackage, MCIlcsoftpackage):
     """A collection of Tracking Relelated Processors Based on MarlinTrk"""
 
-    homepage = "https://github.com/MuonColliderSoft/MarlinTrkProcessors"
-    git      = "https://github.com/MuonColliderSoft/MarlinTrkProcessors.git"
-    url      = "https://github.com/MuonColliderSoft/MarlinTrkProcessors/archive/refs/tags/v02-14-MC.tar.gz"
+    homepage = "https://github.com/madbaron/MarlinTrkProcessors"
+    git      = "https://github.com/madbaron/MarlinTrkProcessors.git"
+    url      = "https://github.com/madbaron/MarlinTrkProcessors/archive/refs/tags/v02-14-MC.tar.gz"
 
     maintainers = ['gianelle', 'pandreetto']
 
     version('master', branch='master')
-    version('2.15.2', sha256='346c8ddd35271a2ca7c09eede101cceb7356a6c1d6f8532cd047a60dc5d067df', preferred=True)
+    version('2.16', branch='cluster', preferred=True)
+    version('2.15.2', sha256='346c8ddd35271a2ca7c09eede101cceb7356a6c1d6f8532cd047a60dc5d067df')
     version('2.15.1', sha256='c8e8ff7bdea755fa1b5f65341a50009cbc05e59ab206c8046d43e169939b5418')
     version("2.15",   sha256="439c2d01bd36c165a2a9e19caeceb807466691a5f240984a2654e2b6d3d0bcbf")
     version('2.14',   sha256='3fda69fbbd23e8e3e7f3c47d898dcd301693286f0a17854e919cfbe68bf3918f')
