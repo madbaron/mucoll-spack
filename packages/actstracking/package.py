dfb5b89d3c8f77f6f8a82cc2445dbf997b0cabb0
Federico Meloni <federico.meloni@cern.ch>
2024-12-10 19:01:41 +0100

    fixing acts sha

--- packages/actstracking/package.py
+++ packages/actstracking/package.py
@@ -16,7 +16,7 @@ class Actstracking(CMakePackage, MCIlcsoftpackage):
     maintainers = ['gianelle', 'kkrizka', 'madbaron']
 
     version('main', branch='main')
-    version('1.3.1', sha256='ce74f55a0ec320284ce8cc2d2d233a7f9c8b912d', preferred=True)
+    version('1.3.1', sha256='3a006120a6e09d4b74ae3357967299001d14209c', preferred=True)
     version('1.3.0', sha256='d013a7700ce453054848572603bcfc6fdf4f5a4d')
     version('1.2.2', sha256='be08b87037167892a9b1a7ad601511beaf99423e836841436c6318fef5fa93de')
     version('1.2.1', sha256='747c15a4c937ab09d79afcc956bb1f1f82ce345febfb4bd18462b71e70ae0b29')
