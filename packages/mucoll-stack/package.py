8b5664789372f4be61f0529e185a822b91e65299
Federico Meloni <federico.meloni@cern.ch>
2025-08-06 18:49:41 +0200

    removed unused lines

--- packages/mucoll-stack/package.py
+++ packages/mucoll-stack/package.py
@@ -149,7 +149,6 @@ class MucollStack(BundlePackage, Key4hepPackage):
         # (see https://github.com/key4hep/key4hep-spack/issues/170)
         env.set("LC_ALL", "C")
         env.set('MUCOLL_STACK', os.path.join(self.spec.prefix, 'setup.sh'))
-        env.set('MUCOLL_GEO', os.path.join(self.spec['lcgeo'].prefix.share.lcgeo.compact, 'MuColl/MuColl_v1/MuColl_v1.xml'))
         env.set('MUCOLL_RELEASE_VERSION', self.spec.version)
 
         # ROOT needs to be in LD_LIBRARY_PATH to prevent using system installations
@@ -161,15 +160,6 @@ class MucollStack(BundlePackage, Key4hepPackage):
             env.prepend_path("CPATH", self.spec["vdt"].prefix.include)
             # When building podio with +rntuple there are warnings constantly without this
             env.prepend_path("LD_LIBRARY_PATH", self.spec["vdt"].libs.directories[0])
-
-        # remove when https://github.com/spack/spack/pull/37881 is merged
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['podio'].libs.directories[0])
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['edm4hep'].libs.directories[0])
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['lcio'].libs.directories[0])
-
-        # remove when https://github.com/spack/spack/pull/38015 is merged
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['dd4hep'].prefix.lib)
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['dd4hep'].prefix.lib64)
 
     def install(self, spec, prefix):
         return install_setup_script(self, spec, prefix, 'MUCOLL_LATEST_SETUP_PATH')
